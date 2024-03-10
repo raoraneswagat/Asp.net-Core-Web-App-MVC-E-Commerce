4b51395ddf812b94087c95585e33ccc0266686f6
raoraneswagat <raorane.swagat@gmail.com>
2024-03-10 11:30:16 +0000

    Add project files.

---
+++ Asp.net Core Web App MVC E Commerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.net_Core_Web_App_MVC_E_Commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
