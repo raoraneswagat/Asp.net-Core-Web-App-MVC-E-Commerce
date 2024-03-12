f7b562a9163f733551f7b355094236d46a13c51e
raoraneswagat <raorane.swagat@gmail.com>
2024-03-12 14:41:17 +0000

    Data Annotations

--- Asp.net Core Web App MVC E Commerce/Models/Category.cs
+++ Asp.net Core Web App MVC E Commerce/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace Asp.net_Core_Web_App_MVC_E_Commerce.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.net_Core_Web_App_MVC_E_Commerce.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
