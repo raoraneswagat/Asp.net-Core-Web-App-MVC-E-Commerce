9d084d264a528b48e29c90adced4771a8108cd4b
raoraneswagat <raorane.swagat@gmail.com>
2024-03-12 14:37:59 +0000

    Category Model

---
+++ Asp.net Core Web App MVC E Commerce/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Asp.net_Core_Web_App_MVC_E_Commerce.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
