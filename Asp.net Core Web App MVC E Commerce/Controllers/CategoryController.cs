d7d887d661173f83c418dd4536d361c61577544d
raoraneswagat <raorane.swagat@gmail.com>
2024-03-12 17:44:35 +0000

    Create Category Controller and View

---
+++ Asp.net Core Web App MVC E Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.net_Core_Web_App_MVC_E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
