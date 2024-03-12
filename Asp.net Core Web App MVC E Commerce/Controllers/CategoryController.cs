c70f6e942362c883dd14c4c7e09b93ae18c67df2
raoraneswagat <raorane.swagat@gmail.com>
2024-03-12 18:17:06 +0000

    Get all categories

--- Asp.net Core Web App MVC E Commerce/Controllers/CategoryController.cs
+++ Asp.net Core Web App MVC E Commerce/Controllers/CategoryController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.net_Core_Web_App_MVC_E_Commerce.Data;
+using Asp.net_Core_Web_App_MVC_E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.net_Core_Web_App_MVC_E_Commerce.Controllers
 {
     public class CategoryController : Controller
     {
+        private ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            List<Category> categories = _db.Category.ToList();
+
             return View();
         }
     }
