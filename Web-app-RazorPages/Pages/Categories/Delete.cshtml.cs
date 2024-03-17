aaf36e0485cbf831c81bbfac07a70dcb5868dbfd
swagat raorane <raorane.swagat@gmail.com>
2024-03-17 15:30:40 +0000

    Razor Delete Category

--- Web-app-RazorPages/Pages/Categories/Delete.cshtml.cs
+++ Web-app-RazorPages/Pages/Categories/Delete.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web_app_RazorPages;
 
 namespace MyApp.Namespace
 {
+    [BindProperties]
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+
+        private ApplicationDbContext _db;
+        public Category category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if(id!=null && id!=0)
+            {
+                category  = _db.Category.Find(id);
+
+            }
+        }
+
+        public IActionResult OnPost(int? id)
+        {
+            if(id!=null && id!=0)
+            {
+                _db.Category.Remove(category);
+                _db.SaveChanges();
+
+                return RedirectToPage("Index");
+
+            }
+
+            return NotFound();
         }
     }
 }
