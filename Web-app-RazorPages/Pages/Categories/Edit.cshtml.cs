db3ddf4230c73b1d0854fcc191b23dc23f3a56e7
swagat raorane <raorane.swagat@gmail.com>
2024-03-17 15:23:09 +0000

    Razor Edit Category

--- Web-app-RazorPages/Pages/Categories/Edit.cshtml.cs
+++ Web-app-RazorPages/Pages/Categories/Edit.cshtml.cs
@@ -1,12 +1,44 @@
+using System.ComponentModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web_app_RazorPages;
 
 namespace MyApp.Namespace
 {
+    [BindProperties]
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private ApplicationDbContext _db;
+
+        public Category category { get; set; }
+        public EditModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+
+            if (id != null && id != 0)
+            {
+                category = _db.Category.Find(id);
+            }
+
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(category);
+                _db.SaveChanges();
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
