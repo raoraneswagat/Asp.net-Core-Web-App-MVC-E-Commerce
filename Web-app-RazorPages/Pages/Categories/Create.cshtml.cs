2c75eef46fb207b0e2261057d6b6cf1675f8da5b
swagat raorane <raorane.swagat@gmail.com>
2024-03-17 15:04:02 +0000

    Razor Create Category

---
+++ Web-app-RazorPages/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web_app_RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class CreateModel : PageModel
+    {
+        private ApplicationDbContext _db;
+        public Category category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost(Category category)
+        {
+              
+              if(ModelState.IsValid)
+              {
+                _db.Category.Add(category);
+                _db.SaveChanges();
+
+                return RedirectToPage("Index");
+              }
+              else{
+                return NotFound();
+              }
+
+        }
+
+    }
+}
