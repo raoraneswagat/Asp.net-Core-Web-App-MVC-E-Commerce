f31663cdd1aaa68cc21e66a775f5ac4b1c90a380
swagat raorane <raorane.swagat@gmail.com>
2024-03-18 21:36:32 +0000

    Combine Product Create and Edit

--- Asp.net Core Web App MVC E Commerce/Areas/Admin/Controllers/ProductController.cs
+++ Asp.net Core Web App MVC E Commerce/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace Asp.net_Core_Web_App_MVC_E_Commerce.Areas.Admin.Controllers
             return View(products);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? Id)
         {
             IEnumerable<SelectListItem> categories = _unitOfWork.Category.GetAll().Select(c => new SelectListItem
             {
@@ -31,8 +31,6 @@ namespace Asp.net_Core_Web_App_MVC_E_Commerce.Areas.Admin.Controllers
                 Value = c.Id.ToString()
 
             });
-            //ViewBag.CategoryList = categories;
-            //ViewData["CategoryList"] = categories;
 
             ProductVM productVM = new()
             {
@@ -40,11 +38,16 @@ namespace Asp.net_Core_Web_App_MVC_E_Commerce.Areas.Admin.Controllers
                 Product = new Product()
             };
 
+            if(Id!=null && Id!=0)
+            {
+                productVM.Product = _unitOfWork.Product.Get(p=>p.Id==Id);
+            }
+
             return View(productVM);
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM,IFormFile? file)
         {
 
             // if (obj.Name == obj.DisplayOrder.ToString())
@@ -72,44 +75,6 @@ namespace Asp.net_Core_Web_App_MVC_E_Commerce.Areas.Admin.Controllers
 
         }
 
-        public IActionResult Edit(int? Id)
-        {
-            if (Id == null || Id == 0)
-            {
-                return NotFound();
-            }
-
-            Product? product = _unitOfWork.Product.Get(c => c.Id == Id);
-
-            if (product == null)
-            {
-                return NotFound();
-            }
-            // _db.Category.FirstOrDefault(c=>c.Id==Id);
-            // _db.Category.Where(w=>w.Id==Id).FirstOrDefault();
-
-            return View(product);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-
-            // if (obj.Name == obj.DisplayOrder.ToString())
-            // {
-            //     ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
-            // }
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["Success"] = "Product Updated Successfully!";
-                return RedirectToAction("Index");
-            }
-            return View();
-
-
-        }
 
         public IActionResult Delete(int? Id)
         {
