acb94ad12e7c9b443211af711d3912f3f71597f2
swagat raorane <raorane.swagat@gmail.com>
2024-03-18 11:47:01 +0000

    Inmplement Repository Interface

---
+++ DataAccess/Repository.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private ApplicationDbContext _db;
+    internal DbSet<T> dbSet;
+
+    public Repository(ApplicationDbContext db)
+    {
+        _db = db;
+        this.dbSet = _db.Set<T>();
+        //_db.Categories == dbSet
+    }
+    public void Add(T entity)
+    {
+        dbSet.Add(entity);
+    }
+
+    public T Get(Expression<Func<T, bool>> filter)
+    {
+        IQueryable<T> query = dbSet;
+        query.Where(filter);
+        return query.FirstOrDefault();
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        IQueryable<T> query = dbSet;
+        return query.ToList();
+    }
+
+    public void Remove(T entity)
+    {
+        dbSet.Remove(entity);
+    }
+
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        dbSet.RemoveRange(entities);
+    }
+}
