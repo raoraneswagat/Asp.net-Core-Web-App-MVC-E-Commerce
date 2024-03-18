4850662af8280f1a841c3635bca37f2dd7ab8c22
swagat raorane <raorane.swagat@gmail.com>
2024-03-18 11:35:50 +0000

    IRepository interface

---
+++ DataAccess/IRepo/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccess;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+public interface IRepository<T> where T : class
+{
+
+    IEnumerable<T> GetAll();
+    T Get(Expression<Func<T, bool>> filter);
+    void Add(T entity);
+    void Remove(T entity);
+    void RemoveRange(T entity);
+
+}
