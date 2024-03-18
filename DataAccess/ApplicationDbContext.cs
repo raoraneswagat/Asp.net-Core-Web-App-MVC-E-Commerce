082faaaf962537b49b6dcdaff0820411a612ddfd
swagat raorane <raorane.swagat@gmail.com>
2024-03-18 16:08:57 +0000

    Add Foreign Key Relation In product

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -35,7 +35,8 @@ public class ApplicationDbContext : DbContext
                 ListPrice = 99,
                 Price = 90,
                 Price50 = 85,
-                Price100 = 80
+                Price100 = 80,
+                CategoryId = 1
             },
             new Product
             {
@@ -47,7 +48,8 @@ public class ApplicationDbContext : DbContext
                 ListPrice = 40,
                 Price = 30,
                 Price50 = 25,
-                Price100 = 20
+                Price100 = 20,
+                CategoryId = 1
             },
             new Product
             {
@@ -59,7 +61,8 @@ public class ApplicationDbContext : DbContext
                 ListPrice = 55,
                 Price = 50,
                 Price50 = 40,
-                Price100 = 35
+                Price100 = 35,
+                CategoryId = 1
             },
             new Product
             {
@@ -71,7 +74,8 @@ public class ApplicationDbContext : DbContext
                 ListPrice = 70,
                 Price = 65,
                 Price50 = 60,
-                Price100 = 55
+                Price100 = 55,
+                CategoryId = 2
             },
             new Product
             {
@@ -83,7 +87,8 @@ public class ApplicationDbContext : DbContext
                 ListPrice = 30,
                 Price = 27,
                 Price50 = 25,
-                Price100 = 20
+                Price100 = 20,
+                CategoryId = 2
             },
             new Product
             {
@@ -95,7 +100,8 @@ public class ApplicationDbContext : DbContext
                 ListPrice = 25,
                 Price = 23,
                 Price50 = 22,
-                Price100 = 20
+                Price100 = 20,
+                CategoryId = 3
             }
         );
     }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models;
 
@@ -34,5 +35,10 @@ public class Product
     [Display(Name = "Price for 100+")]
     [Range(1, 1000)]
     public double Price100 { get; set; }
+
+    public int CategoryId { get; set; }
+
+    [ForeignKey("CategoryId")]
+    public Category Category { get; set; }
 
 }
